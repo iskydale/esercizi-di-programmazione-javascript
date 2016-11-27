5ce5e1d5630b4a38a7ceee12523c183a36403a80
iskydale <gianlucabiosa@gmail.com>
2016-11-27 22:24:26 +0100

    Risolto: Somma e media

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -3,3 +3,15 @@
   Scrivi un programma che prenda in input 5 numeri a tua scelta e restituisca in output la somma e la media.
   http://www.imparareaprogrammare.it
 */
+
+var one = 2;
+var two = 20;
+var three = 1;
+var four = 55;
+var five = 9;
+
+var somma = one + two + three + four + five;
+console.log('La somma dei 5 numeri è: ' +somma);
+
+var media = somma/5;
+console.log('La media è: ' +media);
